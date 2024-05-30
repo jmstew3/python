45416cbba0491f8a0a4c347af2507d6e50c794e6
stewiebeamen <jmstew8@gmail.com>
2024-05-30 17:33:19 -0400

    Fix localhost CORS issue by changing python server ports

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-# app.py
 from flask import Flask, jsonify, request
 import pandas as pd
 import json
 from azure.storage.blob import BlobServiceClient
 from dotenv import load_dotenv
+from flask_cors import CORS
 import os
 
 # Load environment variables from .env file
 load_dotenv()
+
 app = Flask(__name__)
+CORS(app) # Enable CORS for all routes
 
 @app.route('/fetch-draft-data', methods=['GET'])
 def fetch_draft_data():
@@ -18,7 +20,7 @@ def fetch_draft_data():
     return jsonify(json.loads(draft_data_json))
 
 @app.route('/upload-draft-data', methods=['POST'])
-def upload_draft_data():    
+def upload_draft_data():
     connection_string = os.getenv('AZURE_CONNECTION_STRING')
     container_name = 'nfl-draft-data'
     blob_name = 'draft_picks_1980-2024.json'
@@ -27,8 +29,11 @@ def upload_draft_data():
     blob_service_client = BlobServiceClient.from_connection_string(connection_string)
     container_client = blob_service_client.get_container_client(container_name)
     
-    # Create the container if it doesn't exist
-    container_client.create_container()
+    try:
+        # Create the container if it doesn't exist
+        container_client.create_container()
+    except Exception as e:
+        print(f"Container already exists or cannot be created: {e}")
     
     # Get JSON data from request
     draft_data_json = request.get_json()
@@ -40,4 +45,4 @@ def upload_draft_data():
     return jsonify({'message': 'Data uploaded successfully'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
